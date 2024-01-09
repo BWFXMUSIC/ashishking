f620c8f743324bbd7c6bb9a103eb7691a29721d9
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-09 11:51:54 +0530

    Update Youtube.py

--- NihalX/platforms/Youtube.py
+++ NihalX/platforms/Youtube.py
@@ -35,8 +35,8 @@ class YouTubeAPI:
     def __init__(self):
         self.base = "https://www.youtube.com/watch?v="
         self.regex = r"(?:youtube\.com|youtu\.be)"
-        self.status = "https://www.youtube.com/oembed?url="
-        self.listbase = "https://youtube.com/playlist.list="
+        self.status = "https://www.youtube.com/oembed.url="
+        self.listbase = "https://youtube.com/playlist?list="
         self.reg = re.compile(
             r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])"
         )
