003dc478ddf484251286dddbf312bfce0ca08dba
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-10 12:26:11 +0530

    Update Youtube.py

--- NihalX/platforms/Youtube.py
+++ NihalX/platforms/Youtube.py
@@ -4,7 +4,7 @@ import re
 from typing import Union
 
 import aiohttp
-import yt_dlp
+import yt-dlp=2023.12.30
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
