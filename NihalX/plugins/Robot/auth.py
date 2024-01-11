471c980b6c184221fabf59e91877ae6678cf4764
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-11 16:17:17 +0530

    Update auth.py

--- NihalX/plugins/Robot/auth.py
+++ NihalX/plugins/Robot/auth.py
@@ -51,7 +51,7 @@ async def auth(client, message: Message, _):
                 if user.id not in get:
                     get.append(user.id)
             await save_authuser(message.chat.id, token, assis)
-            await message.reply_sticker("CAACAgUAAx0CbqFYzQACC1ljRhqwCP_axVsWWT1mT6-3BHDlygACqwYAAo2jMVZOd4fnecq-ER4E")
+            await message.reply_sticker("CAACAgUAAx0Cfd2c3gACEolln8N3LLqS79Szs87EipGAIYyEqwACXgwAAgQg-VRbh-qSSllGyR4E")
             return await message.reply_text("**» ᴀᴅᴅᴇᴅ ᴛᴏ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ ᴏꜰ ʏᴏᴜʀ ɢʀᴏᴜᴘ.**")
         else:
             await message.reply_text("**» ᴀʟʀᴇᴀᴅʏ ɪɴ ᴛʜᴇ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ.**")
@@ -79,7 +79,7 @@ async def auth(client, message: Message, _):
             if user_id not in get:
                 get.append(user_id)
         await save_authuser(message.chat.id, token, assis)
-        await message.reply_sticker("CAACAgUAAx0CbqFYzQACC1ljRhqwCP_axVsWWT1mT6-3BHDlygACqwYAAo2jMVZOd4fnecq-ER4E")
+        await message.reply_sticker("CAACAgUAAx0Cfd2c3gACEp9ln8T0wBFidk4SGxN9ZO4WIOD15QACxw0AAqH_AAFV3J7K4H2t-H8eBA")
         return await message.reply_text("**» ᴀᴅᴅᴇᴅ ᴛᴏ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ ᴏꜰ ʏᴏᴜʀ ɢʀᴏᴜᴘ.**")
     else:
         await message.reply_text("**» ᴀʟʀᴇᴀᴅʏ ɪɴ ᴛʜᴇ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ.**")
@@ -107,7 +107,7 @@ async def unauthusers(client, message: Message, _):
             if user.id in get:
                 get.remove(user.id)
         if deleted:
-            await message.reply_sticker("CAACAgUAAx0CbqFYzQACC19jRhuBrPg88cC98nkyHPUZO3Ra-wACXQYAAhipMVadDqvIJVdAQB4E")
+            await message.reply_sticker("CAACAgUAAx0Cfd2c3gACEq9ln8VUCgE3Tjrvosp52teqRN198gACDQ0AAi3bAVWiQy77fm5max4E")
             return await message.reply_text("**» ʀᴇᴍᴏᴠᴇᴅ ꜰʀᴏᴍ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ ᴏꜰ ᴛʜɪs ɢʀᴏᴜᴘ.**")
         else:
             return await message.reply_text("**» ᴛᴀʀɢᴇᴛᴇᴅ ᴜsᴇʀ ɪs ɴᴏᴛ ᴀɴ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀ.**")
@@ -119,7 +119,7 @@ async def unauthusers(client, message: Message, _):
         if user_id in get:
             get.remove(user_id)
     if deleted:
-        await message.reply_sticker("CAACAgUAAx0CbqFYzQACC19jRhuBrPg88cC98nkyHPUZO3Ra-wACXQYAAhipMVadDqvIJVdAQB4E")
+        await message.reply_sticker("CAACAgUAAx0Cfd2c3gACErtln8X0Wm41RrHItCATByaczHOnLQACnAwAAjD2AVXnFtUnUUBBJR4E")
         return await message.reply_text("**» ʀᴇᴍᴏᴠᴇᴅ ꜰʀᴏᴍ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀs ʟɪsᴛ ᴏꜰ ᴛʜɪs ɢʀᴏᴜᴘ.**")
     else:
         return await message.reply_text("**» ᴛᴀʀɢᴇᴛᴇᴅ ᴜsᴇʀ ɪs ɴᴏᴛ ᴀɴ ᴀᴜᴛʜᴏʀɪsᴇᴅ ᴜsᴇʀ.**")
